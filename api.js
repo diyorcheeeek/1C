61bedd3c17ea3d0d89ed446ebbcf6cd5e94eddfd
diyorcheeeek <103763302+diyorcheeeek@users.noreply.github.com>
2026-01-04 16:11:33 +0500

    Create api.js

---
+++ api.js
@@ -0,0 +1,13 @@
+// ПОТОМ ЗАМЕНИМ НА REAL API 1C
+
+function getClients(query) {
+  return [];
+}
+
+function getProducts(query) {
+  return [];
+}
+
+function sendOrderTo1C(order) {
+  console.log("Отправка в 1С", order);
+}
